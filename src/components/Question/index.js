308b0423525c03b7e0563ee0ea51be9ace7836b2
Will G <wgrout87@gmail.com>
2022-09-20 13:18:12 -0600

    added button functionality to question page

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Question() {
+    const [answerValues, setAnswerValues] = useState([false, false, false, true])
+
     return (
-        <div class="quiz" id="quiz">
-            <p class="question">Which of the following is NOT a good reason for version control?</p>
-            <div class="quizAnswers">
-                <button class="questionAnswer">Version control allows the codebase to be modified and tested without interrupting the user experience</button>
-                <button class="questionAnswer">Version control allows changes to the codebase to be tested individually</button>
-                <button class="questionAnswer">Version control allows teams to work on individual features synchronously</button>
-                <button class="questionAnswer">Version control allows features to ship directly to the main branch</button>
+        <div className="quiz" id="quiz">
+            <p className="question">Which of the following is NOT a good reason for version control?</p>
+            <div className="quizAnswers">
+                <button
+                    data-index={0}
+                    className="questionAnswer"
+                    onClick={(e) => {
+                        console.log(answerValues[e.target.getAttribute('data-index')]);
+                    }}
+                >
+                    Version control allows the codebase to be modified and tested without interrupting the user experience
+                </button>
+                <button
+                    data-index={1}
+                    className="questionAnswer"
+                    onClick={(e) => {
+                        console.log(answerValues[e.target.getAttribute('data-index')]);
+                    }}
+                >Version control allows changes to the codebase to be tested individually</button>
+                <button
+                    data-index={2}
+                    className="questionAnswer"
+                    onClick={(e) => {
+                        console.log(answerValues[e.target.getAttribute('data-index')]);
+                    }}
+                >Version control allows teams to work on individual features synchronously</button>
+                <button
+                    data-index={3}
+                    className="questionAnswer"
+                    onClick={(e) => {
+                        console.log(answerValues[e.target.getAttribute('data-index')]);
+                    }}
+                >Version control allows features to ship directly to the main branch</button>
             </div>
         </div>
     )
