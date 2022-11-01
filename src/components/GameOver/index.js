fabeb2cec78b9aa2f47e2fb9f68272c0fc34e7db
Will G <wgrout87@gmail.com>
2022-10-31 20:39:06 -0600

    button tracked for entering initials

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
@@ -7,6 +7,9 @@ function GameOver({
     scoreResults,
     setScoreResults,
 }) {
+    const [initial, setInitial] = useState(0);
+    const [btnPress, setBtnPress] = useState("");
+
     return (
         <>
             <h2 className="results">
@@ -16,15 +19,26 @@ function GameOver({
                 {scoreResults}
             </p>
             <div className="initials centered">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>{btnPress}</div>
+                <div>{btnPress}</div>
+                <div>{btnPress}</div>
             </div>
             <h2 className="results">
                 Please enter your initials:
             </h2>
             <div className="btnDiv centered">
-                {letters.map(letter => <button className="charBtn">{letter}</button>)}
+                {letters.map((letter, index) => <button
+                    className="charBtn"
+                    key={index}
+                    onClick={() => {
+                        setBtnPress(letter);
+                        if (initial <= 2) {
+                            setInitial(initial + 1)
+                        }
+                    }}
+                >
+                    {letter}
+                </button>)}
             </div>
         </>
     )
