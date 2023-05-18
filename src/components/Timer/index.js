82e2340f66a836bf20865d6072ea4bb48a70f4a6
Will G <wgrout87@gmail.com>
2023-05-17 22:00:08 -0600

    removed redundant code

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,32 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function Timer({ timeRemaining }) {
-    const [secondPassed, setSecondPassed] = useState(true);
-    const [timerRunning, setTimerRunning] = useState(false);
-
-    // Runs a countdown timer, but is protected agains multiple setTimeout calls
-    function countdown() {
-        setSecondPassed(false);
-        if (!timerRunning && timeRemaining > 0) {
-            setTimerRunning(true);
-            setTimeout(() => {
-                setSecondPassed(true);
-                setTimerRunning(false);
-                countdown();
-            }, 1000)
-        }
-    };
-
-    useEffect(() => {
-        console.log("A new timer instance!")
-    }, []);
-
-    useEffect(() => {
-        if (secondPassed) {
-            countdown();
-        }
-    }, [secondPassed])
-
     return (
         <div className="timeRemaining">
             <p>TIME REMAINING: <br /><span id="timeRemaining">{timeRemaining}</span>
