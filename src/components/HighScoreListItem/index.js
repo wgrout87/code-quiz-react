e2e6d00618947df7c793bd9097b574341c451fb3
Will G <wgrout87@gmail.com>
2023-05-20 13:35:52 -0600

    formatted initials in HighScoreListItem

--- src/components/HighScoreListItem/index.js
+++ src/components/HighScoreListItem/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function HighScoreListItem({ place, fullInitials, score }) {
     return (
         <li className="highScoreListItem">
-            <p>{`${place} ${fullInitials.toString()}`}</p>
+            <p>{`${place} ${fullInitials.toString().replaceAll(",", "")}`}</p>
             <p>{`${score}`}</p>
         </li>
     );
