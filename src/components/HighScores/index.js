35a9237d7118570e0737b27a074d14cc03e526c6
Will G <wgrout87@gmail.com>
2023-05-20 13:03:48 -0600

    added HighScore component

---
+++ src/components/HighScores/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HighScores() {
+    return (
+        <>
+            <h2 class="highScoreListTitle centered">High Scores</h2>
+            <ol>
+
+            </ol>
+        </>
+    );
+};
