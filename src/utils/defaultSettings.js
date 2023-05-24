e31db8151cfdd8b67f11283807e783d27ba99d0c
Will G <wgrout87@gmail.com>
2023-05-23 20:08:35 -0600

    added default settings to exported object

---
+++ src/utils/defaultSettings.js
@@ -0,0 +1,20 @@
+import { questions } from "../../questions";
+
+export const defaultSettings = {
+    questions: questions,
+    quizQuestions: [],
+    currentCategory: 'instructions',
+    visibility: 0,
+    timeRemaining: 180,
+    timerActive: false,
+    timerWidth: '100.00%',
+    timeLeft: 15,
+    combo: 0,
+    pointsMultiplier: 1,
+    score: 0,
+    correctAnswerGiven: true,
+    updateTimer: false,
+    timerBarKey: 0,
+    currentQuestion: 0,
+    fullInitials: []
+}
