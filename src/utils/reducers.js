5083a62b60611462a1b4077e7888de692d9d6a71
Will G <wgrout87@gmail.com>
2023-05-26 06:17:33 -0600

    imported all actions into reducers.js

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -1,11 +1,30 @@
 import { useReducer } from "react";
 import {
     UPDATE_QUIZQUESTIONS,
+    UPDATE_CURRENTCATEGORY,
+    UPDATE_VISIBILITY,
+    UPDATE_TIMEREMAINING,
+    UPDATE_TIMERACTIVE,
+    UPDATE_TIMERWIDTH,
+    UPDATE_TIMELEFT,
+    UPDATE_COMBO,
+    UPDATE_POINTSMULTIPLIER,
+    UPDATE_SCORE,
+    UPDATE_CORRECTANSWERGIVEN,
+    UPDATE_TIMER,
+    UPDATE_TIMERBARKEY,
+    UPDATE_CURRENTQUESTION,
+    UPDATE_FULLINITIALS
 } from "./actions";
 
 export const reducer = (state, action) => {
     switch (action.type) {
         case UPDATE_QUIZQUESTIONS:
+            return {
+                ...state,
+                quizQuestions: action.quizQuestions,
+            };
+        case UPDATE_CURRENTCATEGORY:
             return {
                 ...state,
                 quizQuestions: action.quizQuestions,
