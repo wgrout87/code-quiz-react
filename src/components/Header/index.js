d97e949eaf45ab3fc94971c9a3248b3dae6f5c2e
WGrout <wgrout87@gmail.com>
2023-07-31 16:47:39 -0600

    added site context to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { useSiteContext } from "../../utils/GlobalState";
 
 function Header() {
+    const [state, dispatch] = useSiteContext();
+
     return (
         <header>
             <h1 className="pageTitle">TIMED CODE QUIZ</h1>
             <div className="highScore">
                 <p>CURRENT HIGH SCORE</p>
                 <div className="record">
-                    <p id="highScoreInitials"></p>
-                    <p id="highScorePoints"></p>
+                    <p id="highScoreInitials">{state.highScoreInitials}</p>
+                    <p id="highScorePoints">{state.highScorePoints}</p>
                 </div>
                 <button id="viewTheScoreboard">
                     VIEW THE SCOREBOARD
