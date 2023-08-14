8852b5299191677f37e9235b1c27643012d700d4
WGrout <wgrout87@gmail.com>
2023-08-13 19:51:11 -0600

    added buttons to high scores screen to play again

--- src/components/HighScores/index.js
+++ src/components/HighScores/index.js
@@ -2,9 +2,10 @@ import React, { useEffect } from "react";
 import HighScoreListItem from "../HighScoreListItem";
 import { saveHighScores } from "../../utils/highScores";
 import { useSiteContext } from "../../utils/GlobalState";
+import { PLAY_AGAIN, UPDATE_UPDATETIMER } from "../../utils/actions";
 
-export default function HighScores({ place }) {
-    const [state] = useSiteContext();
+export default function HighScores() {
+    const [state, dispatch] = useSiteContext();
 
     // This useEffect hook looks for changes to the highScores property of the Global State and saves the high scores locally whenever new high scores are achieved.
     useEffect(() => {
@@ -27,6 +28,39 @@ export default function HighScores({ place }) {
                     })
                 }
             </ol>
+            <button
+                className="centered high-scores-btn"
+                onClick={() => {
+                    dispatch({
+                        type: PLAY_AGAIN,
+                        currentCategory: "quiz",
+                        timerActive: true,
+                        visibility: 1
+                    });
+                    setTimeout(() => {
+                        // If there is time remaining, the timer will be updated every second
+                        dispatch({
+                            type: UPDATE_UPDATETIMER,
+                            updateTimer: true
+                        })
+                    }, 1000);
+                }
+                }>
+                Play Again
+            </button>
+            <button
+                className="centered high-scores-btn"
+                onClick={() => {
+                    dispatch({
+                        type: PLAY_AGAIN,
+                        currentCategory: "instructions",
+                        visibility: 0
+                    });
+                }
+                }
+            >
+                Instructions
+            </button>
         </div>
     );
 };
