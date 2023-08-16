37e9a8cc836b8987374fb2be846838a9f41d3c4e
WGrout <wgrout87@gmail.com>
2023-08-16 17:19:41 -0600

    added comments to the instructions component

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -3,7 +3,8 @@ import { useSiteContext } from "../../utils/GlobalState";
 import { BEGIN_QUIZ } from "../../utils/actions"
 
 function Instructions() {
-    const [state, dispatch] = useSiteContext();
+    // Here state was omitted - it was not necessary for this component
+    const [, dispatch] = useSiteContext();
 
     return (
         <>
@@ -17,13 +18,17 @@ function Instructions() {
                 </p>
             </div>
             <div className="beginBtn" id="beginBtn">
+                {/* Clicking this button will begin a new quiz */}
                 <button
                     type="button"
                     onClick={() => {
                         dispatch({
                             type: BEGIN_QUIZ,
+                            // currentCategory being set to 'quiz' will display the Question component within the QuizSpace component
                             currentCategory: "quiz",
+                            // visibility being set to 1 reveals the game content related components (CurrentScore and GameContent components)
                             visibility: 1,
+                            // setting timerActive to true runs the timer for the quiz
                             timerActive: true,
                         });
                     }}
