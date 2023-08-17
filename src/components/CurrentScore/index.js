7b3447a96ea2fb233bdde8c137b277ebc2c7f9b8
WGrout <wgrout87@gmail.com>
2023-08-17 16:38:20 -0600

    added comments to CurrentScore component

--- src/components/CurrentScore/index.js
+++ src/components/CurrentScore/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useSiteContext } from "../../utils/GlobalState";
 
+// This element is the box above the QuizSpace that displays the score while a quiz is ongoing
 function CurrentScore() {
     const [state] = useSiteContext();
     return (
+        // This element begins with visibility of zero - only displays during an ongoing quiz
         <div className="score transition" id="score" style={{ opacity: state.visibility }}>
             <p>CURRENT SCORE:</p>
+            {/* score is dynamically displayed */}
             <p id="currentScore">{state.score}</p>
         </div>
     )
